02f799be4f1bb2d898b6aa54e159a4906023dcc0
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-17 19:28:49 +0900

    19. Ability System Component and Attribute Set

---
+++ Source/DaeRune/Public/AbilitySystem/DRAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "DRAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAERUNE_API UDRAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DaeRune/Public/AbilitySystem/DRAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "DRAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAERUNE_API UDRAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
