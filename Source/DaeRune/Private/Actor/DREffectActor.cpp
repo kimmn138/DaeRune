bb62e3bb15afdbf9702ee71cca136cb7236c497f
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-17 21:43:40 +0900

    38. Effect Actor Improved

--- Source/DaeRune/Private/Actor/DREffectActor.cpp
+++ Source/DaeRune/Private/Actor/DREffectActor.cpp
@@ -2,44 +2,29 @@
 
 
 #include "Actor/DREffectActor.h"
+#include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/DRAttributeSet.h"
-#include "Components/SphereComponent.h"
 
 ADREffectActor::ADREffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
-}
-
-void ADREffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	//TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack!
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UDRAttributeSet* DRAttributeSet = Cast<UDRAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UDRAttributeSet::StaticClass()));
-
-		UDRAttributeSet* MutableDRAttributeSet = const_cast<UDRAttributeSet*>(DRAttributeSet);
-		MutableDRAttributeSet->SetHealth(DRAttributeSet->GetHealth() + 25.f);
-		MutableDRAttributeSet->SetMana(DRAttributeSet->GetMana() - 25.f);
-		Destroy();
-	}
-}
-
-void ADREffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
 void ADREffectActor::BeginPlay()
 {
 	Super::BeginPlay();
+}
 
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ADREffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &ADREffectActor::EndOverlap);
+void ADREffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	if (TargetASC == nullptr) return;
+
+	check(GameplayEffectClass);
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
--- Source/DaeRune/Public/Actor/DREffectActor.h
+++ Source/DaeRune/Public/Actor/DREffectActor.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "DREffectActor.generated.h"
 
-class USphereComponent;
+class UGameplayEffect;
 
 UCLASS()
 class DAERUNE_API ADREffectActor : public AActor
@@ -16,19 +16,12 @@ class DAERUNE_API ADREffectActor : public AActor
 public:
 	ADREffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	UPROPERTY(EditAnywhere, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 };
