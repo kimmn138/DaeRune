50a8382d90d4fed636873a0157a6898b61003b0c
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-17 19:52:21 +0900

    25. Health and Mana

--- Source/DaeRune/Private/AbilitySystem/DRAttributeSet.cpp
+++ Source/DaeRune/Private/AbilitySystem/DRAttributeSet.cpp
@@ -2,4 +2,37 @@
 
 
 #include "AbilitySystem/DRAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UDRAttributeSet::UDRAttributeSet()
+{
+}
+
+void UDRAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UDRAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UDRAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+}
+
+void UDRAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDRAttributeSet, Health, OldHealth);
+}
+
+void UDRAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDRAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UDRAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDRAttributeSet, Mana, OldMana);
+}
+
+void UDRAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDRAttributeSet, MaxMana, OldMaxMana);
+}
