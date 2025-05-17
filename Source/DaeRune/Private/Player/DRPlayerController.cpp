d4be6587dd3c23fabfd6931718329779cb9f8898
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-17 18:27:35 +0900

    10. Aura Player Controller

--- Source/DaeRune/Private/Player/DRPlayerController.cpp
+++ Source/DaeRune/Private/Player/DRPlayerController.cpp
@@ -2,4 +2,27 @@
 
 
 #include "Player/DRPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+ADRPlayerController::ADRPlayerController()
+{
+	bReplicates = true;
+}
+
+void ADRPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(DRContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(DRContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
