3ebbfe2845b8224a8ec45f9b159e1b62f131b76d
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-19 00:03:18 +0900

    110. Aura Projectile Spell

---
+++ Source/DaeRune/Private/AbilitySystem/Abilities/DRProjectileSpell.cpp
@@ -0,0 +1,12 @@
+// Copyright DaeRune
+
+
+#include "AbilitySystem/Abilities/DRProjectileSpell.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void UDRProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("ActivateAbility (C++)"), true, true, FLinearColor::Yellow, 3);
+}
---
+++ Source/DaeRune/Public/AbilitySystem/Abilities/DRProjectileSpell.h
@@ -0,0 +1,19 @@
+// Copyright DaeRune
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/DRGameplayAbility.h"
+#include "DRProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAERUNE_API UDRProjectileSpell : public UDRGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
