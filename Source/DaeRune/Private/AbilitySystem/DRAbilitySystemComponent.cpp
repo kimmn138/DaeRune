ed46c483854f8a1c7809dac9a560bee5edd9abb0
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-18 21:10:44 +0900

    88. Native Gameplay Tags

--- Source/DaeRune/Private/AbilitySystem/DRAbilitySystemComponent.cpp
+++ Source/DaeRune/Private/AbilitySystem/DRAbilitySystemComponent.cpp
@@ -7,14 +7,6 @@
 void UDRAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UDRAbilitySystemComponent::EffectApplied);
-
-	const FDRGameplayTags& GameplayTags = FDRGameplayTags::Get();
-	GEngine->AddOnScreenDebugMessage(
-		-1,
-		10.f,
-		FColor::Orange,
-		FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armor.ToString())
-	);
 }
 
 void UDRAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
