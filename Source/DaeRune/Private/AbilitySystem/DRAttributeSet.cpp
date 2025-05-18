f85cfd981ee451074528fe98bf374f194f2eb3fc
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-18 18:13:38 +0900

    64. Properly Clamping Attributes

--- Source/DaeRune/Private/AbilitySystem/DRAttributeSet.cpp
+++ Source/DaeRune/Private/AbilitySystem/DRAttributeSet.cpp
@@ -42,6 +42,15 @@ void UDRAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallback
 
 	FEffectProperties Props;
 	SetEffectProperties(Data, Props);
+
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+	}
+	if (Data.EvaluatedData.Attribute == GetManaAttribute())
+	{
+		SetMana(FMath::Clamp(GetMana(), 0.f, GetMaxMana()));
+	}
 }
 
 void UDRAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
