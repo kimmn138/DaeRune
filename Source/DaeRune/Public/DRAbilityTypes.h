05c605f3af0c16985b06a5477ad3fbb6810e6f88
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-28 02:11:11 +0900

    153. Struct Ops Type Traits

--- Source/DaeRune/Public/DRAbilityTypes.h
+++ Source/DaeRune/Public/DRAbilityTypes.h
@@ -15,6 +15,29 @@ public:
 		return FGameplayEffectContext::StaticStruct();
 	}
 
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	virtual FGameplayEffectContext* Duplicate() const
+	{
+		FGameplayEffectContext* NewContext = new FGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
+
 	/** Custom serialization, subclasses must override this */
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+};
+
+template<>
+struct TStructOpsTypeTraits<FDRGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FDRGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true
+	};
 };
