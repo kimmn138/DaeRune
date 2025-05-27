aab3e1f5cd2bf7e9be94e7aaebc4639b5b243d8e
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-28 01:37:55 +0900

    145. Implementing Block Chance

--- Source/DaeRune/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/DaeRune/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -3,6 +3,7 @@
 
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 #include "AbilitySystemComponent.h"
+#include "DRGameplayTags.h"
 #include "AbilitySystem/DRAttributeSet.h"
 
 UExecCalc_Damage::UExecCalc_Damage()
@@ -18,4 +19,10 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
 
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	// Get Damage Set by Caller Magnitude
+	float Damage = Spec.GetSetByCallerMagnitude(FDRGameplayTags::Get().Damage);
+
+	const FGameplayModifierEvaluatedData EvaluatedData(UDRAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
