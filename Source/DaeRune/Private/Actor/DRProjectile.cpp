431899d37b02641a5786b395d1a160d5590de35a
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-29 17:42:53 +0900

    210. Rock Impact Effects

--- Source/DaeRune/Private/Actor/DRProjectile.cpp
+++ Source/DaeRune/Private/Actor/DRProjectile.cpp
@@ -49,6 +49,7 @@ void ADRProjectile::Destroyed()
 		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
 		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
 		if (LoopingSoundComponent) LoopingSoundComponent->Stop();
+		bHit = true;
 	}
 	Super::Destroyed();
 }
@@ -68,6 +69,7 @@ void ADRProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AA
 		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
 		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
 		if (LoopingSoundComponent) LoopingSoundComponent->Stop();
+		bHit = true;
 	}
 
 	if (HasAuthority())
