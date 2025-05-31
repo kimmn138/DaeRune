44e566617eac4c30984686655e59a07f2d1b43ae
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-05-31 13:31:12 +0900

    229. Enemies Final Polish

--- Source/DaeRune/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/DaeRune/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -21,7 +21,7 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
 
 	// Get Damage Set by Caller Magnitude
-	float Damage = Spec.GetSetByCallerMagnitude(FDRGameplayTags::Get().Damage);
+	float Damage = Spec.GetSetByCallerMagnitude(FDRGameplayTags::Get().Damage, false);
 
 	const FGameplayModifierEvaluatedData EvaluatedData(UDRAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
