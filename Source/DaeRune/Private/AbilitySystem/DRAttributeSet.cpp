eede82b95a3839b0b17190530485f4fff3b0158c
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-06-05 18:29:10 +0900

    255. Incoming XP Meta Attribute
    DaeRune - 경험치 관련 부분은 추가 안 함

--- Source/DaeRune/Private/AbilitySystem/DRAttributeSet.cpp
+++ Source/DaeRune/Private/AbilitySystem/DRAttributeSet.cpp
@@ -58,7 +58,6 @@ void UDRAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallback
 	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
 	{
 		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
-		UE_LOG(LogTemp, Warning, TEXT("Changed Health on %s, Health: %f"), *Props.TargetAvatarActor->GetName(), GetHealth());
 	}
 	if (Data.EvaluatedData.Attribute == GetIncomingDamageAttribute())
 	{
