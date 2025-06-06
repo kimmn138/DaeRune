20d719554976b9ca66050c6fe153219bfaa0d240
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-06-06 17:42:19 +0900

    304. Debuff Parameters

--- Source/DaeRune/Public/DRGameplayTags.h
+++ Source/DaeRune/Public/DRGameplayTags.h
@@ -41,6 +41,11 @@ public:
 	FGameplayTag Debuff_Arcane;
 	FGameplayTag Debuff_Physical;
 
+	FGameplayTag Debuff_Chance; 
+	FGameplayTag Debuff_Damage;
+	FGameplayTag Debuff_Duration;
+	FGameplayTag Debuff_Frequency;
+
 	FGameplayTag Abilities_Attack;
 	FGameplayTag Abilities_Summon;
 
