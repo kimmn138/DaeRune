b9cc54fde3faee21355a91e2212d64df41bcee9b
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-06-06 16:33:01 +0900

    295. Cost and Cooldown in Spell Description
    DaeRune - 스킬 설명 부분은 추가 안 함

---
+++ Source/DaeRune/Public/AbilitySystem/Abilities/DRFireBolt.h
@@ -0,0 +1,17 @@
+// Copyright DaeRune
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/DRProjectileSpell.h"
+#include "DRFireBolt.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAERUNE_API UDRFireBolt : public UDRProjectileSpell
+{
+	GENERATED_BODY()
+	
+};
