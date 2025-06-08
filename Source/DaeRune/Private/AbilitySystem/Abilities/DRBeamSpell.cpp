c32b6315fed6d826fca99f22f2946b8eefff3580
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-06-09 03:48:28 +0900

    328. First Trace Target

--- Source/DaeRune/Private/AbilitySystem/Abilities/DRBeamSpell.cpp
+++ Source/DaeRune/Private/AbilitySystem/Abilities/DRBeamSpell.cpp
@@ -3,6 +3,7 @@
 
 #include "AbilitySystem/Abilities/DRBeamSpell.h"
 #include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void UDRBeamSpell::StoreCameraDataInfo(const FHitResult& HitResult)
 {
@@ -22,5 +23,37 @@ void UDRBeamSpell::StoreOwnerVariables()
 	if (CurrentActorInfo)
 	{
 		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
+	}
+}
+
+void UDRBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
+{
+	check(OwnerCharacter); 
+	if (OwnerCharacter->Implements<UCombatInterface>())
+	{
+		if (USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon(OwnerCharacter))
+		{
+			TArray<AActor*> ActorsToIgnore;
+			ActorsToIgnore.Add(OwnerCharacter);
+			FHitResult HitResult;
+			const FVector SocketLocation = Weapon->GetSocketLocation(FName("TipSocket"));
+			UKismetSystemLibrary::SphereTraceSingle(
+				OwnerCharacter,
+				SocketLocation,
+				BeamTargetLocation,
+				10.f,
+				TraceTypeQuery1,
+				false,
+				ActorsToIgnore,
+				EDrawDebugTrace::None,
+				HitResult,
+				true);
+
+			if (HitResult.bBlockingHit)
+			{
+				CameraHitLocation = HitResult.ImpactPoint;
+				CameraHitActor = HitResult.GetActor();
+			}
+		}
 	}
 }
