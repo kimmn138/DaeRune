736c116fee2b26a7cfd655f70c582e7fc3db68b9
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-06-09 00:40:10 +0900

    324. GameplayCue Notify Paths

--- Source/DaeRune/Private/AbilitySystem/DRAbilitySystemComponent.cpp
+++ Source/DaeRune/Private/AbilitySystem/DRAbilitySystemComponent.cpp
@@ -37,66 +37,120 @@ void UDRAbilitySystemComponent::AddCharacterPassiveAbilities(const TArray<TSubcl
 
 void UDRAbilitySystemComponent::AbilityInputTagPressed(const FGameplayTag& InputTag)
 {
-	if (!InputTag.IsValid()) return;
+	UE_LOG(LogTemp, Log, TEXT("AbilityInputTagPressed called with InputTag: %s"), *InputTag.ToString());
+	if (!InputTag.IsValid()) return; 
 
 	for (FGameplayAbilitySpec& AbilitySpec : GetActivatableAbilities())
 	{
-		if (AbilitySpec.DynamicAbilityTags.HasTagExact(InputTag))
+		const FString SpecTags = AbilitySpec.GetDynamicSpecSourceTags().ToStringSimple();
+		UE_LOG(LogTemp, Log, TEXT("Checking AbilitySpec Handle=%s, Tags=[%s], IsActive=%s"),
+			*AbilitySpec.Handle.ToString(),
+			*SpecTags,
+			AbilitySpec.IsActive() ? TEXT("true") : TEXT("false"));
+		if (AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(InputTag))
 		{
+			UE_LOG(LogTemp, Log, TEXT("Matched InputTag on AbilitySpec Handle=%s -> Calling AbilitySpecInputPressed"),
+				*AbilitySpec.Handle.ToString());
 			AbilitySpecInputPressed(AbilitySpec);
 			if (AbilitySpec.IsActive())
 			{
 				UGameplayAbility* PrimaryInstance = AbilitySpec.GetPrimaryInstance();
 				if (PrimaryInstance)
 				{
-					InvokeReplicatedEvent(
-						EAbilityGenericReplicatedEvent::InputPressed,
-						AbilitySpec.Handle,
-						PrimaryInstance->GetCurrentActivationInfo().GetActivationPredictionKey()
-					);
+					const auto& PredKey = PrimaryInstance->GetCurrentActivationInfo().GetActivationPredictionKey();
+					UE_LOG(LogTemp, Log, TEXT("PrimaryInstance found (%s). PredictionKey: %s"),
+						*PrimaryInstance->GetName(),
+						*PredKey.ToString());
+					InvokeReplicatedEvent(EAbilityGenericReplicatedEvent::InputPressed, AbilitySpec.Handle, PrimaryInstance->GetCurrentActivationInfo().GetActivationPredictionKey());
+					UE_LOG(LogTemp, Log, TEXT("InvokedReplicatedEvent InputPressed for Handle=%s"),
+						*AbilitySpec.Handle.ToString());
 				}
+				else
+				{
+					UE_LOG(LogTemp, Warning, TEXT("No PrimaryInstance for AbilitySpec Handle=%s"),
+						*AbilitySpec.Handle.ToString());
+				}
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("AbilitySpec Handle=%s is not active after InputPressed"),
+					*AbilitySpec.Handle.ToString());
 			}
 		}
 	}
+
+	UE_LOG(LogTemp, Log, TEXT("AbilityInputTagPressed finished"));
 }
 
 void UDRAbilitySystemComponent::AbilityInputTagHeld(const FGameplayTag& InputTag)
 {
+	UE_LOG(LogTemp, Log, TEXT("AbilityInputTagHeld called with InputTag: %s"), *InputTag.ToString());
 	if (!InputTag.IsValid()) return;
 
 	for (FGameplayAbilitySpec& AbilitySpec : GetActivatableAbilities())
 	{
-		if (AbilitySpec.DynamicAbilityTags.HasTagExact(InputTag))
+		const FString SpecTags = AbilitySpec.GetDynamicSpecSourceTags().ToStringSimple();
+		UE_LOG(LogTemp, Log, TEXT("Checking AbilitySpec Handle=%s, Tags=[%s], IsActive=%s"),
+			*AbilitySpec.Handle.ToString(),
+			*SpecTags,
+			AbilitySpec.IsActive() ? TEXT("true") : TEXT("false"));
+		if (AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(InputTag))
 		{
+			UE_LOG(LogTemp, Log, TEXT("Matched InputTag on AbilitySpec Handle=%s -> Calling AbilitySpecInputPressed"),
+				*AbilitySpec.Handle.ToString());
 			AbilitySpecInputPressed(AbilitySpec);
+			UE_LOG(LogTemp, Log, TEXT("AbilitySpecInputPressed called for Handle=%s"), *AbilitySpec.Handle.ToString());
 			if (!AbilitySpec.IsActive())
 			{
-				TryActivateAbility(AbilitySpec.Handle);
+				UE_LOG(LogTemp, Log, TEXT("AbilitySpec Handle=%s is not active, calling TryActivateAbility"), *AbilitySpec.Handle.ToString());
+				bool bActivated = TryActivateAbility(AbilitySpec.Handle);
+				UE_LOG(LogTemp, Log, TEXT("TryActivateAbility returned %s for Handle=%s"),
+					bActivated ? TEXT("true") : TEXT("false"),
+					*AbilitySpec.Handle.ToString());
+			}
+			else
+			{
+				UE_LOG(LogTemp, Log, TEXT("AbilitySpec Handle=%s was already active, skipping activation"), *AbilitySpec.Handle.ToString());
 			}
 		}
 	}
+	UE_LOG(LogTemp, Log, TEXT("AbilityInputTagHeld finished"));
 }
 
 void UDRAbilitySystemComponent::AbilityInputTagReleased(const FGameplayTag& InputTag)
 {
+	UE_LOG(LogTemp, Log, TEXT("AbilityInputTagReleased called with InputTag: %s"), *InputTag.ToString());
 	if (!InputTag.IsValid()) return;
 
 	for (FGameplayAbilitySpec& AbilitySpec : GetActivatableAbilities())
 	{
-		if (AbilitySpec.DynamicAbilityTags.HasTagExact(InputTag))
+		const FString SpecTags = AbilitySpec.GetDynamicSpecSourceTags().ToStringSimple();
+		UE_LOG(LogTemp, Log, TEXT("Checking AbilitySpec Handle=%s, Tags=[%s], IsActive=%s"),
+			*AbilitySpec.Handle.ToString(),
+			*SpecTags,
+			AbilitySpec.IsActive() ? TEXT("true") : TEXT("false"));
+		if (AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(InputTag) && AbilitySpec.IsActive())
 		{
+			UE_LOG(LogTemp, Log, TEXT("Matched InputTag on AbilitySpec Handle=%s"), *AbilitySpec.Handle.ToString());
 			AbilitySpecInputReleased(AbilitySpec);
+			UE_LOG(LogTemp, Log, TEXT("Called AbilitySpecInputReleased for Handle=%s"), *AbilitySpec.Handle.ToString());
 			UGameplayAbility* PrimaryInstance = AbilitySpec.GetPrimaryInstance();
 			if (PrimaryInstance)
 			{
-				InvokeReplicatedEvent(
-					EAbilityGenericReplicatedEvent::InputReleased,
-					AbilitySpec.Handle,
-					PrimaryInstance->GetCurrentActivationInfo().GetActivationPredictionKey()
-				);
+				const auto& PredKey = PrimaryInstance->GetCurrentActivationInfo().GetActivationPredictionKey();
+				UE_LOG(LogTemp, Log, TEXT("PrimaryInstance found (%s). PredictionKey: %s"),
+					*PrimaryInstance->GetName(),
+					*PredKey.ToString());
+				InvokeReplicatedEvent(EAbilityGenericReplicatedEvent::InputReleased, AbilitySpec.Handle, PrimaryInstance->GetCurrentActivationInfo().GetActivationPredictionKey());
+				UE_LOG(LogTemp, Log, TEXT("InvokedReplicatedEvent InputReleased for Handle=%s"), *AbilitySpec.Handle.ToString());
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("No PrimaryInstance for AbilitySpec Handle=%s"), *AbilitySpec.Handle.ToString());
 			}
 		}
 	}
+	UE_LOG(LogTemp, Log, TEXT("AbilityInputTagReleased finished"));
 }
 
 void UDRAbilitySystemComponent::ForEachAbility(const FForEachAbility& Delegate)
