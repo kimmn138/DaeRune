090abbfa52ec8cf783455b8bed5fcf46e051cc22
kimmn138 <127746860+kimmn138@users.noreply.github.com>
2025-06-08 14:56:45 +0900

    316. FireBolt Projectile Spread

--- Source/DaeRune/Public/AbilitySystem/Abilities/DRFireBolt.h
+++ Source/DaeRune/Public/AbilitySystem/Abilities/DRFireBolt.h
@@ -14,4 +14,14 @@ class DAERUNE_API UDRFireBolt : public UDRProjectileSpell
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION(BlueprintCallable)
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "FireBolt")
+	float ProjectileSpread = 90.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "FireBolt")
+	int32 MaxNumProjectiles = 5;
 };
